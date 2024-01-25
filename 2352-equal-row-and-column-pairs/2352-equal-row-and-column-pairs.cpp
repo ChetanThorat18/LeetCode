07a72bb91a8e04e32cbf1c533fea53aef79f7bd1
ChetanThorat18 <139691562+ChetanThorat18@users.noreply.github.com>
2024-01-25 23:49:33 +0530

    Time: 144 ms (30.62%), Space: 26.3 MB (82.84%) - LeetHub

---
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int equalPairs(vector<vector<int>>& grid) {
+        // Brute Force : For each row check every column O(n^3)
+        int n = grid.size();
+        int count = 0;
+
+        // Fix the row
+        for(int row=0; row<n; row++){
+            // Fix the column
+            for(int col = 0; col<n; col++){
+                bool isEqual = true;
+                // Check each element in the row and corresponding column
+                for(int k=0; k<n; k++){
+                    if( grid[row][k] != grid[k][col]){
+                        isEqual = false;
+                        break;
+                    }
+                }
+                // If the row and column are equal, increment the count
+                if(isEqual){
+                    count++;
+                }
+            }
+        }
+
+        return count;
+
+    }
+};
