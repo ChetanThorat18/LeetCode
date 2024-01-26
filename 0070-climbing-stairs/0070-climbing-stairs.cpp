efbf1da6d707dcae45662e7a1edc5cf5c627458b
ChetanThorat18 <139691562+ChetanThorat18@users.noreply.github.com>
2024-01-26 19:14:54 +0530

    Update 0070-climbing-stairs.cpp

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -26,3 +26,28 @@ public:
         return climbStairs(n, dp);
     }
 };
+
+/*
+    class Solution {
+    public int climbStairs(int n) {
+        if (n <= 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        } else if (n == 2) {
+            return 2;
+        }
+
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n];
+    }
+    
+}
+*/
