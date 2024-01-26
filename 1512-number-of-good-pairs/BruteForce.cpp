73dda6baa995c51f2a678b17f84ab6e4b8376a12
ChetanThorat18 <139691562+ChetanThorat18@users.noreply.github.com>
2024-01-26 23:06:34 +0530

    Create BruteForce.cpp

---
+++ 1512-number-of-good-pairs/BruteForce.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int numIdenticalPairs(vector<int>& nums) {
+        // Brute Force using nested loops
+        int count = 0;
+
+        for(int i=0; i<nums.size()-1; i++){
+            for(int j=i+1; j<nums.size(); j++){
+                if(nums[i] == nums[j]){
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+};
