957c97bdb05cae63ae0f7b0cf8610f4627ad63dc
ChetanThorat18 <139691562+ChetanThorat18@users.noreply.github.com>
2024-03-11 00:31:30 +0530

    Time: 0 ms (100.00%), Space: 12.5 MB (61.63%) - LeetHub

---
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        // Approach 4 : Two Pointer Approach
+        int n1 = nums1.size();
+        int n2 = nums2.size();
+
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+
+        vector<int> result;
+
+        int i = 0, j = 0; // Pointers to traverse nums1 and nums2
+
+        // Traverse both arrays to find common elements
+        while (i < nums1.size() && j < nums2.size()) {
+            if (nums1[i] == nums2[j]) {
+                // If the elements at current positions are equal, it's a common element
+                result.push_back(nums1[i]);
+
+                // Skip over any duplicates in nums1
+                while (i < nums1.size() - 1 && nums1[i] == nums1[i + 1]) {
+                    i++;
+                }
+
+                // Skip over any duplicates in nums2
+                while (j < nums2.size() - 1 && nums2[j] == nums2[j + 1]) {
+                    j++;
+                }
+
+                // Move to the next elements in both arrays
+                i++;
+                j++;
+            } else if (nums1[i] < nums2[j]) {
+                // If nums1[i] is smaller, move to the next element in nums1
+                i++;
+            } else {
+                // If nums2[j] is smaller, move to the next element in nums2
+                j++;
+            }
+        }
+
+        return result;
+    }
+};
+
+/*
+    Time Complexity :
+*/
