415621b0b23caa99acd20194f7b2fdc5a586fb8e
ChetanThorat18 <139691562+ChetanThorat18@users.noreply.github.com>
2024-03-24 00:03:52 +0530

    Time: 9 ms (28.62%), Space: 9.4 MB (17.45%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        // two maps to store mappings from s to t and from t to s
+        unordered_map<char, char> s_to_t_mapping;
+        unordered_map<char, char> t_to_s_mapping;
+
+        // Iterate through each character in strings s and t
+        for (int i = 0; i < s.length(); ++i) {
+            // Get the current characters from strings s and t
+            char char_s = s[i];
+            char char_t = t[i];
+
+            // Check if the current character from s has been mapped to a character in t before
+            if (s_to_t_mapping.find(char_s) != s_to_t_mapping.end()) {
+                // If it has been mapped before, check if the previous mapping is consistent
+                if (s_to_t_mapping[char_s] != char_t)
+                    return false; // Return false if the mapping is inconsistent
+            } else {
+                // If the current character from s has not been mapped before, add it to the mapping
+                s_to_t_mapping[char_s] = char_t;
+            }
+            
+
+            // Check if the current character from t has been mapped to a character in s before
+            if (t_to_s_mapping.find(char_t) != t_to_s_mapping.end()) {
+                // If it has been mapped before, check if the previous mapping is consistent
+                if (t_to_s_mapping[char_t] != char_s)
+                    return false; // Return false if the mapping is inconsistent
+            } else {
+                // If the current character from t has not been mapped before, add it to the mapping
+                t_to_s_mapping[char_t] = char_s;
+            }
+        }
+
+        return true;
+    }
+};
