559a1cd21b4737eb2b4fd6e0bf00dfc9156eca71
ChetanThorat18 <139691562+ChetanThorat18@users.noreply.github.com>
2024-03-24 23:17:11 +0530

    Update 0290-word-pattern.cpp

--- 0290-word-pattern/0290-word-pattern.cpp
+++ 0290-word-pattern/0290-word-pattern.cpp
@@ -1,7 +1,9 @@
 class Solution {
 public:
     bool wordPattern(string pattern, string s) {
-
+        // Initialize two unordered maps:
+        // charToWordMap: Maps each character in the pattern to its corresponding word in string s.
+        // wordToCharMap: Maps each word in string s to its corresponding character in the pattern.
         unordered_map<char, string> charToWordMap;
         unordered_map<string, char> wordToCharMap;
 
@@ -13,32 +15,39 @@ public:
             words.push_back(word);
         }
         
+        // If the number of characters in the pattern does not match the number of words in s, return false
         if(pattern.size() != words.size())
             return false;
 
+        // Iterate through each character in the pattern
         for (int i = 0; i < pattern.size(); i++) {
             char ch = pattern[i];
             string word = words[i];
 
+            // Check if the current character is already mapped to a word
             if (charToWordMap.find(ch) != charToWordMap.end()) {
-
+                // If yes, check if the mapped word matches the current word
                 if (charToWordMap[ch] != words[i]) {
-                    return false;
+                    return false; // If not, return false
                 }
             } else {
+                // If not, map the current character to the current word
                 charToWordMap[ch] = words[i];
             }
 
+            // Check if the current word is already mapped to a character
             if (wordToCharMap.find(word) != wordToCharMap.end()) {
-
+                // If yes, check if the mapped character matches the current character
                 if (wordToCharMap[word] != ch) {
-                    return false;
+                    return false; // If not, return false
                 }
             } else {
+                // If not, map the current word to the current character
                 wordToCharMap[word] = ch;
             }
         }
 
+        // If all characters in the pattern and their corresponding words in s form a bijection, return true
         return true;
     }
 };
