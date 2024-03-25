a51b3294c7ea65bcf8186bf52e978e76897f81f3
ChetanThorat18 <139691562+ChetanThorat18@users.noreply.github.com>
2024-03-25 18:36:21 +0530

    Update 0058-length-of-last-word.cpp

--- Leetcode_Problems/0058-length-of-last-word/0058-length-of-last-word.cpp
+++ Leetcode_Problems/0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -3,13 +3,19 @@ public:
     int lengthOfLastWord(string s) {
         int n = s.size();
         int i=n-1;
-        while(s[i]==' '&& i>=0){
+        // Skip trailing spaces from the end of the string
+        while (s[i] == ' ' && i >= 0) {
             i--;
         }
-        int length = 0;
-        while(i>=0 && s[i]!=' ' ){
-            length++;
-            i--;
+
+        int length = 0; 
+
+        // Calculate the length of the last word
+        while (i >= 0 && s[i] != ' ') {
+            // Increment length for each non-space character
+            length++; 
+            // Move to the previous character
+            i--;  
         }
 
         return length;
