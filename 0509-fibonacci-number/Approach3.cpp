12c6b40eadd1ca4fb379c2f3369c1d73d1d831ec
ChetanThorat18 <139691562+ChetanThorat18@users.noreply.github.com>
2024-04-27 22:13:53 +0530

    Create Approach3.cpp

---
+++ 0509-fibonacci-number/Approach3.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int fib(int n) {
+        // Approach 3 : Bottom Up DP
+
+        if(n == 0)
+            return 0;
+        if(n == 1)
+            return 1;
+
+        vector<int>dp(n+1 , -1);
+
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for(int i=2; i<=n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
+        return dp[n];
+    }
+};
