01c9c8904fc0b0e0b463e7afceb83edb3dd89d44
ChetanThorat18 <139691562+ChetanThorat18@users.noreply.github.com>
2024-06-07 23:11:35 +0530

    Create Recursion.cpp

---
+++ 0926-flip-string-to-monotone-increasing/Recursion.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    int n;
+
+    // Recursive function to determine the minimum flips needed to make the string monotone increasing
+    int solve(string &s, int curr_idx, int prev_val) {
+        // Base case: If we've processed all characters in the string, no more flips are needed
+        if (curr_idx >= n)
+            return 0;
+
+        int flip = INT_MAX;    
+        int no_flip = INT_MAX; 
+
+        // Check the current character in the string
+        if (s[curr_idx] == '0') {
+            if (prev_val == 0) {
+                // If previous value was '0', we have two options:
+                // 1. Flip current '0' to '1' and move to next character with previous value as '1'
+                flip = 1 + solve(s, curr_idx+1, 1);
+                // 2. Keep current '0' as '0' and move to next character with previous value as '0'
+                no_flip = solve(s, curr_idx+1, 0);
+            } else {
+                // If previous value was '1', we must flip current '0' to '1' to maintain monotonicity
+                flip = 1 + solve(s, curr_idx+1, 1);
+            }
+        } else if (s[curr_idx] == '1') {
+            if (prev_val == 0) {
+                // If previous value was '0', we have two options:
+                // 1. Flip current '1' to '0' and move to next character with previous value as '0'
+                flip = 1 + solve(s, curr_idx+1, 0);
+                // 2. Keep current '1' as '1' and move to next character with previous value as '1'
+                no_flip = solve(s, curr_idx+1, 1);
+            } else {
+                // If previous value was '1', we simply move to the next character with previous value as '1'
+                no_flip = solve(s, curr_idx+1, 1);
+            }
+        }
+
+        // Return the minimum of the two options: flipping or not flipping the current character
+        return min(flip, no_flip);
+    }
+
+    int minFlipsMonoIncr(string s) {
+        // Approach : Recursion
+        n = s.length(); 
+        // Start the recursion with the initial index 0 and previous value as '0'(eg- for string to be monotonic it should start with 0)
+        return solve(s, 0, 0); 
+    }
+};
