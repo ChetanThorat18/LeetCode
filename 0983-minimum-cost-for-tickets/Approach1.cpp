330e6290685ec1079ca2c109b03beb9d2c139e07
ChetanThorat18 <139691562+ChetanThorat18@users.noreply.github.com>
2024-06-10 23:36:09 +0530

    Create Approach1.cpp

---
+++ 0983-minimum-cost-for-tickets/Approach1.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int n;
+
+    int solve(vector<int>& days, vector<int> costs, int idx) {
+        // Base case: If we've covered all travel days, no cost is needed
+        if (idx >= n)
+            return 0;
+
+        // Calculate the cost if we buy a 1-day pass for the current day
+        int one_day_pass_cost = costs[0] + solve(days, costs, idx + 1);
+
+        // Find the index for which the 7-day pass will be valid
+        int j = idx;
+        int pass_valid_till = days[idx] + 7;
+        while (j < n && days[j] < pass_valid_till) {
+            j++;
+        }
+        // Calculate the cost if we buy a 7-day pass for the current day
+        int seven_day_pass_cost = costs[1] + solve(days, costs, j);
+
+        // Find the index for which the 30-day pass will be valid
+        j = idx;
+        pass_valid_till = days[idx] + 30;
+        while (j < n && days[j] < pass_valid_till) {
+            j++;
+        }
+        // Calculate the cost if we buy a 30-day pass for the current day
+        int thirty_day_pass_cost = costs[2] + solve(days, costs, j);
+
+        // Return the minimum cost among the three options
+        return min({one_day_pass_cost, seven_day_pass_cost, thirty_day_pass_cost});
+    }
+
+    int mincostTickets(vector<int>& days, vector<int>& costs) {
+        // Approach : Recursion
+        n = days.size();
+
+        // Start the recursive process from the first day
+        return solve(days, costs, 0);
+    }
+};
