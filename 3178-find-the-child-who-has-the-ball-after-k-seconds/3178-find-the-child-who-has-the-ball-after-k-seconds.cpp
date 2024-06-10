2f3aea1be68ba2f52fcf24f3d862d97f548bcf89
ChetanThorat18 <139691562+ChetanThorat18@users.noreply.github.com>
2024-06-10 22:20:44 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (83.33%) - LeetHub

---
+++ 3178-find-the-child-who-has-the-ball-after-k-seconds/3178-find-the-child-who-has-the-ball-after-k-seconds.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int numberOfChild(int n, int k) {
+        // Approach : Simple Simulation as per problem
+        int position = 0;
+        int direction = 1;
+    
+    for (int i = 0; i < k; ++i) {
+        position += direction;
+        
+        // Reverse direction if the ball reaches either end
+        if (position == n - 1) {
+            direction = -1;
+        } else if (position == 0) {
+            direction = 1;
+        }
+    }
+    
+        return position;
+    }
+};
