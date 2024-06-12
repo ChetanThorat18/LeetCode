48024cdb7c707a803a9cb0a188ec231257c20f66
ChetanThorat18 <139691562+ChetanThorat18@users.noreply.github.com>
2024-06-12 23:23:04 +0530

    Create Approach1.cpp

---
+++ 0516-longest-palindromic-subsequence/Approach1.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int n;
+
+    int LCS(string s1, string s2, int i, int j) {
+        // If either index goes out of bounds, return 0 (base case)
+        if (i >= n || j >= n)
+            return 0;
+
+        // If characters match, add 1 to the result and move both indices forward
+        if (s1[i] == s2[j])
+            return 1 + LCS(s1, s2, i + 1, j + 1);
+
+        // If characters do not match, find the maximum of moving either index forward
+        return max(LCS(s1, s2, i, j + 1), LCS(s1, s2, i + 1, j));
+    }
+
+    int longestPalindromeSubseq(string s1) {
+        n = s1.length(); 
+        string s2 = s1; // Copy the input string
+        reverse(s2.begin(), s2.end()); // Reverse the copied string to get the reverse of s1
+
+        // Find the length of the longest common subsequence between s1 and its reverse
+        return LCS(s1, s2, 0, 0);
+    }
+};
