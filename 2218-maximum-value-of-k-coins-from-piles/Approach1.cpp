d7a654c5b474dbd6932b5cf36ff3a03fc9f949c1
ChetanThorat18 <139691562+ChetanThorat18@users.noreply.github.com>
2024-06-13 13:27:23 +0530

    Create Approach1.cpp

---
+++ 2218-maximum-value-of-k-coins-from-piles/Approach1.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int n;
+
+   
+    // piles: the 2D vector of piles
+    // i: current pile index
+    // k: number of coins left to pick
+    int solve(vector<vector<int>>& piles, int i, int k){
+
+        // Base case: if we have processed all piles, return 0
+        if (i >= n)
+            return 0;
+
+        // Case 1: Do not take any coins from the current pile
+        int not_taken = solve(piles, i+1, k);
+
+        // Case 2: Take coins from the current pile
+        int taken = 0;
+        int sum = 0;
+        
+        // Iterate over the possible number of coins to take from the current pile
+        for (int j = 0; j < min((int)piles[i].size(), k); j++) {
+            // Add the value of the current coin to the sum
+            sum += piles[i][j];
+
+            // Calculate the maximum value by taking the current sum and the result of the recursive call
+            taken = max(taken, sum + solve(piles, i+1, k - (j + 1)));
+        }
+
+        // Return the maximum value between taking and not taking coins from the current pile
+        return max(taken, not_taken);
+    }
+
+    int maxValueOfCoins(vector<vector<int>>& piles, int k) {
+        // Approach : Recursion
+        n = piles.size();
+
+        // Call the recursive function starting from the first pile and k coins
+        return solve(piles, 0, k);
+    }
+};
