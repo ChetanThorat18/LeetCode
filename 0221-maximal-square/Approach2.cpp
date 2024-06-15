8bb9f79ff3bc79388c14178a1ee6386fb6e41935
ChetanThorat18 <139691562+ChetanThorat18@users.noreply.github.com>
2024-06-16 00:08:45 +0530

    Create Approach2.cpp

---
+++ 0221-maximal-square/Approach2.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+    int m;         // Number of rows in the matrix
+    int n;         // Number of columns in the matrix
+    int maxSide;   // Maximum side length of the square found so far
+    
+    vector<vector<int>>dp;
+
+    int solve(vector<vector<char>>& matrix, int i, int j) {
+
+        // Base case: If we are out of bounds, return 0
+        if (i >= m || j >= n) {
+            return 0;
+        }
+
+        if(dp[i][j] != -1)
+            return dp[i][j];
+
+        // Recursive calls to get the sizes of squares  at right, bottom, and bottom-right cells
+        int right = solve(matrix, i, j + 1);
+        int bottom = solve(matrix, i + 1, j);
+        int diagonal = solve(matrix, i + 1, j + 1);
+
+        // If the current cell is '1', calculate the side length of the square starting at (i, j)
+        if (matrix[i][j] == '1') {
+            // The side length of the square ending at (i, j) is 1 plus the minimum of the side lengths of squares
+            // ending at the right, bottom, and bottom-right cells
+            int side = 1 + min({right, bottom, diagonal});
+            
+            // Update the maximum side length found so far
+            maxSide = max(maxSide, side);
+            
+            // Return the side length of the square ending at (i, j)
+            return dp[i][j] = side;
+        } else {
+            // If the current cell is '0', it can't be part of a square of '1's, so return 0
+            return dp[i][j] = 0;
+        }
+    }
+
+    int maximalSquare(vector<vector<char>>& matrix) {
+        // Approach : Recursion  + Memoization
+
+        // Initialize the dimensions of the matrix
+        m = matrix.size();
+        n = matrix[0].size();
+
+        dp.assign(m, vector<int>(n,-1));
+
+        maxSide = 0; // Initialize maxSide to 0
+
+        // Start the recursion from the top-left corner of the matrix
+        solve(matrix, 0, 0);
+
+        // Calculate and return the area of the largest square found
+        return maxSide * maxSide;
+    }
+};
