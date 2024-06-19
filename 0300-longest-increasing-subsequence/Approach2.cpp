fe066181033245d43a9e1ed1685fbed45aaf2338
ChetanThorat18 <139691562+ChetanThorat18@users.noreply.github.com>
2024-06-19 23:34:23 +0530

    Create Approach2.cpp

---
+++ 0300-longest-increasing-subsequence/Approach2.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    int n;
+
+    vector<vector<int>>dp;
+
+    // Recursive function to find the length of the longest increasing subsequence
+    // idx: the current index in the nums array
+    // prevIdx: the index of the previous element in the subsequence
+    int solve(vector<int>& nums, int idx, int prevIdx) {
+        // Base case: If we have processed all elements, return 0
+        if (idx >= n)
+            return 0;
+
+        if(prevIdx != -1 && dp[idx][prevIdx] != -1)
+            return dp[idx][prevIdx];
+
+        // Initialize the length of the subsequence if we take the current element
+        int take = 0;
+
+        // Check if we can include the current element in the subsequence
+        if (prevIdx == -1 || nums[prevIdx] < nums[idx]) {
+            // Recursively find the length of the subsequence including the current element
+            take = 1 + solve(nums, idx + 1, idx);
+        }
+
+        // Recursively find the length of the subsequence excluding the current element
+        int skip = solve(nums, idx + 1, prevIdx);
+
+        // Return the maximum length of the subsequence obtained by either including or excluding the current element
+
+        if(prevIdx != -1)
+            return dp[idx][prevIdx] = max(take, skip);
+
+        return max(take, skip);
+    }
+
+    int lengthOfLIS(vector<int>& nums) {
+        // Approach : Recursion + Memoization
+        n = nums.size();
+
+        dp.assign(n, vector<int>(n,-1));
+
+        // Start the recursion from the first element with no previous element
+        return solve(nums, 0, -1);
+    }
+};
