09d8c45f2845ead3c2ec5c49ce97535026f6eb75
ChetanThorat18 <139691562+ChetanThorat18@users.noreply.github.com>
2024-06-21 19:00:08 +0530

    Create Approach1.cpp

---
+++ 1035-uncrossed-lines/Approach1.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int m; // size of nums1
+    int n; // size of nums2
+
+    int solve(vector<int>& nums1, vector<int>& nums2, int i, int j){
+        // Base case: If we have reached the end of either array, return 0
+        if(i >= m || j >= n)
+            return 0;
+
+        // If the current elements of nums1 and nums2 are equal
+        if(nums1[i] == nums2[j])
+            // Include this pair and recursively find the next pair
+            return 1 + solve(nums1, nums2, i+1, j+1);
+
+        // If the current elements are not equal, explore two options:
+        // 1. Fix i and move j forward
+        int fix_i = solve(nums1, nums2, i, j+1);
+        // 2.  Fix j and move i forward
+        int fix_j = solve(nums1, nums2, i+1, j);
+
+        // Return the maximum of the two options above
+        return max(fix_i, fix_j);
+    }
+
+    int maxUncrossedLines(vector<int>& nums1, vector<int>& nums2) {
+        // Approach : Recursion
+        m = nums1.size(); 
+        n = nums2.size(); 
+
+        // Call the recursive function starting from index 0 of both arrays
+        return solve(nums1, nums2, 0, 0);
+    }
+};
